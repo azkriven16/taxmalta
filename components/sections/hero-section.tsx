51b7db0a4dc3742cf1350780e9d9590e1f723355
Euger <azkriven17@gmail.com>
2025-09-10 12:17:22 +0800

    added hero section

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -1,8 +1,85 @@
 import { HoverEffect } from "../ui/card-hover-effect";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Star } from "lucide-react";
+
+const reviews = {
+  count: 200,
+  rating: 5.0,
+  avatars: [
+    {
+      src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-1.webp",
+      alt: "Avatar 1",
+    },
+    {
+      src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-2.webp",
+      alt: "Avatar 2",
+    },
+    {
+      src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-3.webp",
+      alt: "Avatar 3",
+    },
+    {
+      src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-4.webp",
+      alt: "Avatar 4",
+    },
+    {
+      src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-5.webp",
+      alt: "Avatar 5",
+    },
+  ],
+};
 
 export function HeroSection() {
   return (
     <main className="container mx-auto flex flex-col items-center justify-center space-y-20 p-8 pt-24">
+      <section className="py-32">
+        <div className="container text-center">
+          <div className="mx-auto flex max-w-5xl flex-col gap-6">
+            <h1 className="text-3xl font-extrabold lg:text-6xl">
+              No More Guessing Your Tax — Use Our Free Malta Calculator
+            </h1>
+            <p className="text-muted-foreground text-balance lg:text-lg">
+              Instantly calculate your personal income tax in Malta based on the
+              latest tax brackets. Whether you&apos;re employed, self-employed,
+              or a pensioner, our tool gives you a clear breakdown of what you
+              owe.
+            </p>
+            <p className="text-muted-foreground text-balance lg:text-lg">
+              Try our calculators — no sign-in required.
+            </p>
+          </div>
+          <Button asChild size="lg" className="mt-5">
+            <a href="#">Try Now for Free</a>
+          </Button>
+          <div className="mx-auto mt-10 flex w-fit flex-col items-center gap-4 sm:flex-row">
+            <span className="mx-4 inline-flex items-center -space-x-4">
+              {reviews.avatars.map((avatar, index) => (
+                <Avatar key={index} className="size-14 border">
+                  <AvatarImage src={avatar.src} alt={avatar.alt} />
+                </Avatar>
+              ))}
+            </span>
+            <div>
+              <div className="flex items-center gap-1">
+                {[...Array(5)].map((_, index) => (
+                  <Star
+                    key={index}
+                    className="size-5 fill-yellow-400 text-yellow-400"
+                  />
+                ))}
+                <span className="mr-1 font-semibold">
+                  {reviews.rating?.toFixed(1)}
+                </span>
+              </div>
+              <p className="text-muted-foreground text-left font-medium">
+                from {reviews.count}+ reviews
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section>
         <div className="text-center text-balance">
           <h1 className="text-3xl font-bold">Top of the UK tax calculators</h1>
