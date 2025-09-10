be9f81e285c126b1af440456277fa5d9b3ed007a
Euger <azkriven17@gmail.com>
2025-09-10 11:33:47 +0800

    added demo calculators

---
+++ components/ui/card-hover-effect.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "motion/react";
+import { useState } from "react";
+
+export const HoverEffect = ({
+  items,
+  className,
+}: {
+  items: {
+    title: string;
+    description: string;
+    link: string;
+    image: string;
+  }[];
+  className?: string;
+}) => {
+  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 py-10 md:grid-cols-2 lg:grid-cols-3",
+        className,
+      )}
+    >
+      {items.map((item, idx) => (
+        <a
+          href={item?.link}
+          key={item?.link}
+          className="group relative block aspect-[16/10] h-full w-full p-2 md:aspect-[9/12]"
+          onMouseEnter={() => setHoveredIndex(idx)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === idx && (
+              <motion.span
+                className="absolute inset-0 block h-full w-full rounded-3xl bg-neutral-200 dark:bg-slate-800/[0.8]"
+                layoutId="hoverBackground"
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 0.15 },
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, delay: 0.2 },
+                }}
+              />
+            )}
+          </AnimatePresence>
+          <Card>
+            <CardImage src={item.image} alt={item.title} />
+            <CardTitle>{item.title}</CardTitle>
+            <CardDescription>{item.description}</CardDescription>
+          </Card>
+        </a>
+      ))}
+    </div>
+  );
+};
+
+const Card = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "bg-secondary relative z-20 flex h-full w-full flex-col items-center overflow-hidden rounded-2xl border p-4 group-hover:border-slate-700 dark:border-white/[0.2]",
+        className,
+      )}
+    >
+      <div className="relative z-50 w-full text-center">
+        <div className="flex flex-col items-center justify-around gap-2">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CardImage = ({
+  src,
+  alt,
+  className,
+}: {
+  src: string;
+  alt: string;
+  className?: string;
+}) => {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className={cn(
+        "aspect-[16/6] rounded-lg object-cover md:aspect-video",
+        className,
+      )}
+    />
+  );
+};
+
+const CardTitle = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <h4
+      className={cn("text-xl font-bold tracking-wide text-balance", className)}
+    >
+      {children}
+    </h4>
+  );
+};
+
+const CardDescription = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <p
+      className={cn(
+        "text-muted-foreground text-sm leading-relaxed tracking-wide text-balance",
+        className,
+      )}
+    >
+      {children}
+    </p>
+  );
+};
